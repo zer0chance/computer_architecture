7f1ae2ef6ecae3e9190165d3b71a376f8e8bfadd
zer0chance <ier7928@gmail.com>
2020-02-16 15:57:26 +0700

    Create myTerm.h header

--- lab2/main.c
+++ lab2/main.c
@@ -1,8 +1,26 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "myTerm.h"
 
-int main ()
+
+int main(void) 
 {
-    printf("\n\nNumber: %d\n\n", ~5);
-    return 0;      
+    mt_clrscr();
+    
+    mt_gotoXY(20, 20);
+
+    int rows, cols;
+    if (!mt_getscreensize(&rows, &cols))
+        printf("\n Rows: %d\n Cols: %d\n", rows, cols);
+    else
+        printf("\n Can`t get screen size.\n");
+    
+    if (mt_setfgcolor(FG_GREEN)) printf("\n Can`t set this fg color.\n");
+    if (mt_setbgcolor(BG_BLUE)) printf("\n Can`t set this bg color.\n");
+    printf("\n\nTEST TEXT\n\n");    
+
+    return 0;
 }
+
+
+
+
+
