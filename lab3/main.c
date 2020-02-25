5cba3acdfc1858e744a149cf0443e1e4b6a07ff2
zer0chance <ier7928@gmail.com>
2020-02-26 00:09:25 +0700

    Create functions to set big chars

--- lab3/main.c
+++ lab3/main.c
@@ -11,16 +11,30 @@
 
 int main()
 {
-    //bc_printA("ccc");
-    bc_box(8, 30, 4, 8);
-    bc_box(5, 20, 40, 20);
+    bc_printA("\n\nabc\n\n");
+    bc_box(4, 8, 8, 4);
+    bc_box(2, 4, 60, 30);
     
 
-    // int n1 = 4286677503;
-    // int n2 = 2172748287;
-    // int char1[2] = {n1, n2};
-    // bc_printbigchar(char1, 10, 20, 0, 0);
-    int i = -2147483648;
+    int n1 = 4286677503;
+    int n2 = 2172748287;
+    int char1[2] = {n1, n2};
+    bc_printbigchar(char1, 10, 10, 36, 43);
+
+    // int char2[2] = {0, 0};
+    bc_setbigcharpos(char1, 1, 1, 0);
+    bc_setbigcharpos(char1, 1, 8, 0);
+    bc_setbigcharpos(char1, 8, 8, 0);
+    bc_setbigcharpos(char1, 8, 1, 0);
+    bc_setbigcharpos(char1, 3, 4, 1);
+    bc_setbigcharpos(char1, 6, 4, 1);
+    bc_printbigchar(char1, 20, 10, 36, 43);
+
     //printf("\n%d\n", (n1 >> 14) );
+    printf("\033[50;1H");
+    int value;
+    bc_getbigcharpos(char1, 1, 2, &value);
+    printf("\n (1; 2) = %d \n\n", value);
+
     return 0;
 }
--- lab3/myBigChars.h
+++ lab3/myBigChars.h
@@ -74,7 +74,12 @@ int bc_printbigchar(int bc[2], int x, int y, int fgcolor, int bgcolor)
     {
         if (x <= ws.ws_row - 8 && y <= ws.ws_col - 8)
         { 
-            printf("\033[%d;%dH\033(0", x, y);
+            if (fgcolor >= 30 && fgcolor < 38 && bgcolor >= 40 && bgcolor < 48)
+            {
+                printf("\033[%d;%dH\033(0\033[%d;%dm", x, y, fgcolor, bgcolor);
+            }
+            else 
+                return EXIT_FAILURE;
             for (int k = 0; k < 2; k++)
             {
                 int mask = 31;
@@ -96,7 +101,7 @@ int bc_printbigchar(int bc[2], int x, int y, int fgcolor, int bgcolor)
                 }
             }    
             
-            printf("\033(B");
+            printf("\033(B\033[0m");
             return EXIT_SUCCESS;
         }
         else
@@ -104,4 +109,70 @@ int bc_printbigchar(int bc[2], int x, int y, int fgcolor, int bgcolor)
     }
     else 
         return EXIT_FAILURE;
+}
+
+
+int bc_setbigcharpos(int bc[2], int x, int y, int value) // x and y starts is in range 1..8
+{
+    if (x > 0 && x <= 8 && y > 0 && y <= 8)
+    {
+        int i, pos;
+        if (x > 4)
+        {
+            i = 1;
+            x -= 4;
+        }
+        else 
+            i = 0;
+        switch (value)
+        {
+            case 0:
+                pos = (x - 1) * 8 + y; 
+                bc[i] &= ~(1 << (32 - pos));    
+                return EXIT_SUCCESS;
+                break;
+            case 1:
+                pos = (x - 1) * 8 + y; 
+                bc[i] |= (1 << (32 - pos)); // Because we count coordinates from left to right in number    
+                return EXIT_SUCCESS;
+            default:
+                return EXIT_FAILURE;
+                break;
+        }
+    }
+    else 
+        return EXIT_FAILURE;
+}
+
+
+int bc_getbigcharpos(int bc[2], int x, int y, int *value)
+{
+    if (x > 0 && x <= 8 && y > 0 && y <= 8)
+    {
+        int i, pos;
+        if (x > 4)
+        {
+            i = 1;
+            x -= 4;
+        }
+        else 
+            i = 0;
+        pos = (x - 1) * 8 + y;     
+        *value = (bc[i] & (1 << (32 - pos))) >> (32 - pos);     
+        return EXIT_SUCCESS; 
+    }
+    else 
+        return EXIT_FAILURE;
+}
+
+
+int bc_bigcharwrite(int fd, int* big, int count)
+{
+    //TODO
+}
+
+
+int bc_bigcharread(int fd, int* big,int need_count, int* count)
+{
+    //TODO
 }
