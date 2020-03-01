0cebb5f20dd85b08d9e9c476f08cb662a56eabc2
zer0chance <ier7928@gmail.com>
2020-03-01 22:51:44 +0700

    Create directory for simple computer

---
+++ simple_computer/main.c
@@ -0,0 +1,156 @@
+#include "myReadkey.h"
+#include "myBigChars.h"
+#include "myTerm.h"
+#include "mySimpleComputer.h"
+
+// f5 - ^[[15~
+// f6 - ^[[17~
+
+// 11111111
+// 11000011
+// 11000011
+// 11111111
+// 00000011
+// 00000110
+// 00001100
+// 00011000
+
+int bc_chars[12][2] =
+{
+    {4286677377, 2172748287},  // 0
+    {253704039, 117901063},    // 1
+    {4278387459, 4290822399},  // 2
+    {4278387711, 50529279},    // 3
+    {3284386755, 4278387459},  // 4
+    {4290822399, 50529279},    // 5
+    {4290822399, 3284386815},  // 6
+    {4278387462, 202116108},   // 7
+    {4286677503, 2172748287},  // 8
+    {4291019775, 50727960},    // 9
+    {404232447, 4279769112},   // +
+    {255, 4278190080}          // -
+};
+
+int bc_plus[2] = {404232447, 4279769112};
+int bc_minus[2] = {255, 4278190080};
+int bc_zero[2] = {4286677377, 2172748287};
+
+
+void print_ic_content()
+{
+    int x = 3;
+    bc_printbigchar(bc_plus, 17, x, 0, 0);
+    bc_printbigchar(bc_chars[RAM[IC] / 1000], 17, 12, 0, 0);
+    bc_printbigchar(bc_chars[(RAM[IC] % 1000) / 100], 17, 21, 0, 0);
+    bc_printbigchar(bc_chars[(RAM[IC] % 100) / 10], 17, 30, 0, 0);
+    bc_printbigchar(bc_chars[RAM[IC] % 10], 17, 39, 0, 0);
+}
+
+
+void print_flags()
+{
+    mt_gotoXY(14, 71);
+    int value;
+    sc_regGet(WRONG_OPCODE, &value);
+    if (value)
+        printf("W ");
+
+    sc_regGet(CLOCK_IGNORE, &value);
+    if (value)
+        printf("C ");
+
+    sc_regGet(MEM_OUT_OF_BOUND, &value);
+    if (value)
+        printf("M ");
+
+    sc_regGet(ZERO_DIVISION, &value);
+    if (value)
+        printf("Z ");
+
+    sc_regGet(OP_OVERFLOW, &value);
+    if (value)
+        printf("O ");                
+}
+
+
+void print_term()
+{
+    bc_box(4, 2, 62, 12);
+    mt_gotoXY(4, 29);
+    printf("Memory");
+    for (int i = 0, k = 0, x = 5, y = 3; i < 9; i++)
+    {
+        mt_gotoXY(x++, y);
+        for (int j = 0; j < 10; j++)
+        {
+            printf("+%-4d ", RAM[k++]);
+        }
+        //printf("\n");
+    }
+
+    bc_box(4, 65, 20, 3);
+    mt_gotoXY(4, 69);
+    printf("accumulator");
+
+    bc_box(7, 65, 20, 3);
+    mt_gotoXY(7, 66);
+    printf("instructionCounter");
+    mt_gotoXY(8, 72);
+    printf("+%-4d", IC);
+
+    bc_box(10, 65, 20, 3);
+    mt_gotoXY(10, 70);
+    printf("Operation");
+
+    bc_box(13, 65, 20, 3);
+    mt_gotoXY(13, 72);
+    printf("Flags");
+    print_flags(); 
+
+    bc_box(16, 2, 47, 10);
+    print_ic_content();
+
+    bc_box(16, 50, 35, 10);
+    mt_gotoXY(16, 52);
+    printf("Keys:");
+
+    mt_gotoXY(17, 51);
+    printf("l  - load");
+    mt_gotoXY(18, 51);
+    printf("s  - save");
+    mt_gotoXY(19, 51);
+    printf("r  - run");
+    mt_gotoXY(20, 51);
+    printf("t  - step");
+    mt_gotoXY(21, 51);
+    printf("i  - reset");
+    mt_gotoXY(22, 51);
+    printf("F5 - accumulator");
+    mt_gotoXY(23, 51);
+    printf("F6 - inctructionCounter");
+}
+
+
+int main()
+{
+    // int key;
+    // rk_readkey(&key);
+    // printf("\n\nReaded: %d \n\n", key);
+
+    // bc_box(10, 5, 20, 10);
+    // mt_gotoXY(10, 11);
+    // printf("Memory");
+
+    sc_memorySet(IC, 3245);
+    IC = 44;
+    sc_memorySet(IC, 23);
+
+    sc_regSet(WRONG_OPCODE, 1);
+    sc_regSet(CLOCK_IGNORE, 1);
+    sc_regSet(OP_OVERFLOW, 1);
+
+    print_term();
+
+    mt_gotoXY(40, 1);
+    return 0;
+}
---
+++ simple_computer/myBigChars.h
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// enum
+// {
+//     FG_BLACK   = 30,
+//     FG_RED     = 31, 
+//     FG_GREEN   = 32,
+//     FG_YELLOW  = 33, 
+//     FG_BLUE    = 34, 
+//     FG_MAGENTA = 35, 
+//     FG_CYAN    = 36, 
+//     FG_WHITE   = 37
+// };
+
+
+// enum
+// {
+//     BG_BLACK   = 40,
+//     BG_RED     = 41, 
+//     BG_GREEN   = 42,
+//     BG_YELLOW  = 43, 
+//     BG_BLUE    = 44, 
+//     BG_MAGENTA = 45, 
+//     BG_CYAN    = 46, 
+//     BG_WHITE   = 47
+// };
+
+
+int bc_printA(char* str)
+{
+    printf("\033(0%s\033(B", str);
+    return EXIT_SUCCESS;
+}
+
+
+int bc_box(int x1, int y1, int x2, int y2)
+{
+    struct winsize ws;
+    if (!ioctl(1, TIOCGWINSZ, &ws))
+    {
+        if (x1 <= ws.ws_row && y2 <= ws.ws_col)
+        { 
+            printf("\033[%d;%dH\033(0", x1, y1);
+            printf("┌");
+            for (int i = 1; i < x2 - 1; i++)
+                printf("─");
+            printf("┐");
+            for (int i = 1; i < y2 - 1; i++)
+            {
+                printf("\033[%d;%dH│", ++x1, y1);
+                printf("\033[%d;%dH│", x1, y1 + x2 - 1);    
+            }
+            printf("\033[%d;%dH└", ++x1, y1);
+            for (int i = 1; i < x2 - 1; i ++)
+                printf("─");
+            printf("┘\033(B");
+            return EXIT_SUCCESS;
+        }
+        else
+            return EXIT_FAILURE; 
+    }
+    else 
+        return EXIT_FAILURE;
+}
+
+
+int bc_printbigchar(int bc[2], int x, int y, int fgcolor, int bgcolor)
+{
+    struct winsize ws;
+    if (!ioctl(1, TIOCGWINSZ, &ws))
+    {
+        if (x <= ws.ws_row - 8 && y <= ws.ws_col - 8)
+        { 
+            if (fgcolor == 0 || bgcolor == 0)
+            {
+                printf("\033[%d;%dH\033(0", x, y);                
+            }
+            else if (fgcolor >= 30 && fgcolor < 38 && bgcolor >= 40 && bgcolor < 48)
+            {
+                printf("\033[%d;%dH\033(0\033[%d;%dm", x, y, fgcolor, bgcolor);
+            }
+            else 
+                return EXIT_FAILURE;
+            for (int k = 0; k < 2; k++)
+            {
+                int mask = 31;
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if ((bc[k] >> mask) & 1) //TODO: need fix
+                        {
+                            printf("▒"); 
+                        }
+                        else 
+                        {
+                            printf(" "); 
+                        } 
+                        mask--; 
+                    }
+                    printf("\033[%d;%dH", ++x, y);
+                }
+            }    
+            
+            printf("\033(B\033[0m");
+            return EXIT_SUCCESS;
+        }
+        else
+            return EXIT_FAILURE; 
+    }
+    else 
+        return EXIT_FAILURE;
+}
+
+
+int bc_setbigcharpos(int bc[2], int x, int y, int value) // x and y starts is in range 1..8
+{
+    if (x > 0 && x <= 8 && y > 0 && y <= 8)
+    {
+        int i, pos;
+        if (x > 4)
+        {
+            i = 1;
+            x -= 4;
+        }
+        else 
+            i = 0;
+        switch (value)
+        {
+            case 0:
+                pos = (x - 1) * 8 + y; 
+                bc[i] &= ~(1 << (32 - pos));    
+                return EXIT_SUCCESS;
+                break;
+            case 1:
+                pos = (x - 1) * 8 + y; 
+                bc[i] |= (1 << (32 - pos)); // Because we count coordinates from left to right in number    
+                return EXIT_SUCCESS;
+            default:
+                return EXIT_FAILURE;
+                break;
+        }
+    }
+    else 
+        return EXIT_FAILURE;
+}
+
+
+int bc_getbigcharpos(int bc[2], int x, int y, int *value)
+{
+    if (x > 0 && x <= 8 && y > 0 && y <= 8)
+    {
+        int i, pos;
+        if (x > 4)
+        {
+            i = 1;
+            x -= 4;
+        }
+        else 
+            i = 0;
+        pos = (x - 1) * 8 + y;     
+        *value = (bc[i] & (1 << (32 - pos))) >> (32 - pos);     
+        return EXIT_SUCCESS; 
+    }
+    else 
+        return EXIT_FAILURE;
+}
+
+
+int bc_bigcharwrite(int fd, int bc[2], int count) 
+{
+    int result = write(fd, (int*)bc, count); 
+    printf("\nWritten: %d chars\n", result); 
+
+    return EXIT_SUCCESS;    
+}
+
+
+int bc_bigcharread(int fd, int bc[2], int* count) 
+{
+    *count = read(fd, (int*)bc, 8); 
+
+    return EXIT_SUCCESS; 
+}
