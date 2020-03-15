c8fa08e07d5db70c877513071ff0b97af6cef244
zer0chance <ier7928@gmail.com>
2020-03-15 13:01:12 +0700

    Create myReadkey.h

--- lab4/main.c
+++ lab4/main.c
@@ -1,2 +1,97 @@
-// f5 - ^[[15~
-// f6 - ^[[17~
+// #include <unistd.h> 
+// #include <stdlib.h> 
+// #include <fcntl.h> 
+// #include <termios.h> 
+// #include <stdio.h> 
+// int main ( int argc, char **argv ) { 
+//    struct termios savetty, tty; 
+//    char ch; 
+//    int x, y; 
+//    printf( "Enter start position (x y): " ); 
+//    scanf( "%d %d", &x, &y ); 
+//    if( !isatty( 0 ) ) { 
+//       fprintf( stderr, "stdin not terminal\n" ); 
+//       exit( EXIT_FAILURE ); 
+//    }; 
+//    tcgetattr( 0, &tty );             // получили состояние терминала 
+//    savetty = tty; 
+//    tty.c_lflag &= ~( ICANON | ECHO | ISIG ); 
+//    tty.c_cc[ VMIN ] = 1; 
+//    tcsetattr( 0, TCSAFLUSH, &tty ); // изменили состояние терминала 
+//    printf( "%c[2J", 27 );           // очистили экран 
+//    fflush( stdout ); 
+//    printf( "%c[%d;%dH", 27, y, x ); // установили курсор в позицию 
+//    fflush( stdout ); 
+//    for( ; ; ) { 
+//       read( 0, &ch, 1 ); 
+//       if( ch == 'q' ) break; 
+//       switch( ch ) { 
+//       case 'j': 
+//          printf( "%c[1A", 27 ); 
+//          break;
+//       case 'k': 
+//          printf( "%c[1B", 27 ); 
+//          break;
+//       case 'l': 
+//          printf( "%c[1C", 27 ); 
+//          break;
+//       case 'h': 
+//          printf( "%c[1D", 27 ); 
+//          break;
+//       }; 
+//       fflush( stdout ); 
+//    }; 
+//    tcsetattr( 0, TCSAFLUSH, &savetty ); // восстановили состояние терминала 
+//    printf( "\n" ); 
+//    exit( EXIT_SUCCESS ); 
+// } 
+
+
+
+
+
+
+
+
+#include "myReadkey.h"
+
+
+int main()
+{
+    rk_mytermsave();
+    rk_mytermregime(OFF, 0, 1, OFF, OFF);
+
+    printf( "\033[2J"); // cls
+    fflush(stdout);
+
+    int ch;
+
+    for(;;)
+    {
+        rk_readkey(&ch);
+        if (ch == 'q') break;
+
+        switch (ch)
+        {
+        case 'k':
+            printf("\033[1A");
+            break;
+        case 'j':
+            printf("\033[1B");
+            break;
+        case 'l':
+            printf("\033[1C");
+            break;
+        case 'h':
+            printf("\033[1D");
+            break;        
+        }
+
+        fflush(stdout);
+    }
+
+    if(rk_mytermrestore()) printf("Failed to restore terminal state!\n\n");
+    
+
+    return 0;
+}
--- lab4/myReadkey.h
+++ lab4/myReadkey.h
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <termios.h>
+
+#define ON 1
+#define OFF 0
+
+struct termios term_state;
+
 
 int rk_readkey(int* key)
 {
@@ -21,17 +27,45 @@ int rk_readkey(int* key)
 
 int rk_mytermsave(void)
 {
-
+    if(!tcgetattr(0, &term_state))
+        return EXIT_SUCCESS;
+    else
+        return EXIT_FAILURE;
 }
 
 
 int rk_mytermrestore(void)
 {
-
+    if(!tcsetattr(0, TCSAFLUSH, &term_state))
+        return EXIT_SUCCESS;
+    else
+        return EXIT_FAILURE;
 }
 
 
 int rk_mytermregime(int regime, int vtime, int vmin, int echo, int sigint)
 {
+    struct termios ts;
+    if(regime) 
+        ts.c_lflag |= ICANON;
+    else 
+        ts.c_lflag &= ~ICANON;
 
+    ts.c_cc[VTIME] = vtime;
+    ts.c_cc[VMIN] = vmin;
+
+    if(echo)
+        ts.c_lflag |= ECHO;
+    else
+        ts.c_lflag &= ~ECHO;
+    
+    if(sigint)
+        ts.c_lflag |= ISIG;
+    else
+        ts.c_lflag &= ~ISIG;
+
+    if(tcsetattr(0, TCSAFLUSH, &ts))
+        return EXIT_FAILURE;
+    else
+        return EXIT_SUCCESS;            
 }
