297f8ba8db8b461b293365d86c547fcaacb2b78e
zer0chance <ier7928@gmail.com>
2020-05-23 13:56:31 +0700

    Make disk table

--- lab6/lab7.h
+++ lab6/lab7.h
@@ -1,133 +1,457 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-#define BITS_IN_GIGABYTE 8589934592
-
-#define CHS_SECTOR_SIZE     20
-#define LARGE_SECTOR_SIZE   24
-#define IDECHS_SECTOR_SIZE  28
-#define LBA_SECTOR_SIZE     32
-
-
-typedef struct CHS
+typedef struct
 {
-    __uint16_t C;
-    __uint16_t H;
-    __uint16_t S; // 20 bit
-
-    double capacity;
+    uint64_t C : 10; 
+    uint64_t H : 4;
+    uint64_t S : 6;
 } tCHS;
 
-
-typedef struct LARGE
+typedef struct
 {
-    __uint16_t C;
-    __uint16_t H;
-    __uint16_t S; // 24 bit
-    
-    double capacity;
+    uint64_t C : 10; 
+    uint64_t H : 8;
+    uint64_t S : 6;
 } tLARGE;
 
-
-typedef struct IDECHS
+typedef struct
 {
-    __uint16_t C;
-    __uint16_t H;
-    __uint16_t S;  // 28 bit
-
-    double capacity;
+    uint64_t C : 16; 
+    uint64_t H : 4;
+    uint64_t S : 8;
 } tIDECHS;
 
+typedef uint64_t tLBA;
 
-typedef struct LBA
+typedef struct
 {
-    __uint16_t C;
-    __uint16_t H;
-    __uint16_t S; // 32 bit
-    
-    double capacity;
-} tLBA;
+    tLARGE startCHS;
+    tLARGE endCHS;
+    tLBA startLBA;
+
+    uint32_t size;
+    uint8_t  active;
+    uint8_t  type;
+} tPartition;
 
 
-void set_chs_capacity(tCHS* d)
+int g_lba2chs (tLBA, tCHS *);
+int g_lba2large (tLBA, tLARGE *);
+int g_lba2idechs (tLBA, tIDECHS *);
+int g_chs2large (tCHS, tLARGE *);
+int g_chs2idechs (tCHS, tIDECHS *);
+int g_chs2lba (tCHS, tLBA *);
+int g_large2chs (tLARGE, tCHS *);
+int g_large2idechs (tLARGE, tIDECHS *);
+int g_large2lba (tLARGE, tLBA *);
+int g_idechs2chs (tIDECHS, tCHS *);
+int g_idechs2lagre (tIDECHS, tLARGE *);
+int g_idechs2lba (tIDECHS, tLBA *);
+
+int a_lba2chs (tCHS, tLBA, tCHS *);
+int a_lba2large (tLARGE, tLBA, tLARGE *);
+int a_lba2idechs (tIDECHS, tLBA, tIDECHS *);
+int a_chs2lba (tCHS, tCHS, tLBA *);
+int a_large2lba (tLARGE, tLARGE, tLBA *);
+int a_idechs2lba (tIDECHS, tIDECHS, tLBA *);
+int a_large2chs (tLARGE, tCHS, tLARGE, tCHS *);
+int a_large2idechs (tLARGE, tIDECHS, tLARGE, tIDECHS *);
+int a_chs2large (tCHS, tLARGE, tCHS, tLARGE *);
+int a_idechs2large (tIDECHS, tLARGE, tIDECHS, tLARGE *);
+int a_chs2idechs (tCHS, tIDECHS, tCHS, tIDECHS *);
+int a_idechs2chs (tIDECHS, tCHS, tIDECHS, tCHS *);
+
+
+int g_lba2chs (tLBA g1, tCHS *g2)
 {
-    d->capacity = d->C * d->H * d->S * CHS_SECTOR_SIZE / BITS_IN_GIGABYTE; 
+    int L1;
+    int nlog;
+    int k = 2;
+
+    L1 = g1 / 63;
+
+    nlog = L1 / 1023;
+
+    while (nlog > k)
+    {
+        if (k == 16) break;
+        k *= 2;
+    }
+
+    if (k == 16)
+        k = 15;
+
+    if (nlog / k < 1024) 
+    {
+        g2->C = L1 / k;
+        g2->H = k;
+        g2->S = 63;
+    } 
+    else
+        return 0;
+
+    return 1;
 }
 
-
-void set_large_capacity(tLARGE* d)
+int g_lba2large (tLBA g1, tLARGE *g2)
 {
-    d->capacity = d->C * d->H * d->S * LARGE_SECTOR_SIZE / BITS_IN_GIGABYTE; 
+    int L1;
+    int nlog;
+    int k = 2;
+
+    L1 = g1 / 63;
+
+    nlog = L1 / 1024;
+
+    while (nlog / k > 0)
+    {
+        if (k == 256) break;
+        k *= 2;
+    }
+
+    if (k==256)
+        k=255;
+
+    if (nlog / k < 1024)
+    {
+        g2->C = L1 / k;
+        g2->H = k;
+        g2->S = 63;
+    } else 
+        return 0;
+
+    return 1;
 }
 
-
-void set_idechs_capacity(tIDECHS* d)
+int g_lba2idechs (tLBA g1, tIDECHS *g2)
 {
-    d->capacity = (double)(d->C * d->H * d->S * IDECHS_SECTOR_SIZE) / BITS_IN_GIGABYTE; 
+    int L1;
+    int nlog;
+    int k = 2;
+
+    L1 = g1 / 255;
+
+    nlog = L1 / 65535;
+
+    while (nlog / k > 0)
+    {
+        if (k == 16) break;
+        k *= 2;
+    }
+
+    if(k == 16) k = 15;
+
+    if (nlog / k < 65536)
+    {
+        g2->C=L1/k;
+        g2->H=k;
+        g2->S=25;
+    } else 
+        return 0;
+
+    return 1;
 }
 
-q
-void set_lba_capacity(tLBA* d)
+int g_chs2large (tCHS g1, tLARGE *g2)
 {
-    d->capacity = d->C * d->H * d->S * LBA_SECTOR_SIZE / BITS_IN_GIGABYTE; 
+    int c,h;
+    c=g1.C;
+    h=g1.H;
+
+    do {
+        c /= 2;
+        h *= 2;
+    } while ((c / 2 > 0) && (h * 2 < 256));
+
+    g2->C=c;
+    g2->H=h;
+    g2->S=g1.S;
+
+    return 1;
 }
 
-
-int g_lba2chs (tLBA old, tCHS* new)
+int g_chs2idechs (tCHS g1, tIDECHS *g2) 
 {
+    int c,h;
 
+    c = g1.C;
+    h = g1.H;
+
+    do {
+        c *= 2;
+        h /= 2;
+    } while ((c * 2 < 65536) && (h / 2 > 0));
+
+    g2->C = c;
+    g2->H = h;
+    g2->S = g1.S;
+
+    return 1;
 }
 
-int g_lba2large (tLBA old, tLARGE* new)
+int g_chs2lba (tCHS g1, tLBA *g2)
 {
+    *g2 = g1.C * g1.H * g1.S;
 
-} 
-
-int g_lba2idechs (tLBA old, tIDECHS *new)
-{
-
+    return 1;
 }
 
-int g_chs2large (tCHS old,  tLARGE* new)
+int g_large2chs (tLARGE g1, tCHS *g2)
 {
+    int c,h;
+    c = g1.C;
+    h = g1.H;
 
+    do {
+        c *= 2;
+        h /= 2;
+    } while ((c*2<1024) && (h / 2 > 0));
+
+    if (c > 1023 || h > 15) return 0;
+
+    g2->C = c;
+    g2->H = h;
+    g2->S = g1.S;
+
+    return 1;
 }
 
-int g_chs2lba(tCHS old, tLBA* new)
+int g_large2idechs (tLARGE g1, tIDECHS *g2)
 {
+    int c,h;
 
+    c=g1.C;
+    h=g1.H;
+
+    do {
+        c*=2;
+        h/=2;
+    } while ((c * 2 < 1024) && (h / 2 > 0));
+
+    if (c > 65536 || h > 15) return 0;
+
+    g2->C = c;
+    g2->H = h;
+    g2->S = g1.S;
+
+    return 1;
 }
 
-int g_chs2idechs (tIDECHS old, tLBA* new)
+int g_large2lba (tLARGE g1, tLBA *g2)
 {
+    *g2 = g1.C * g1.H * g1.S;
 
+    return 1;
 }
 
-int g_large2chs (tLARGE old, tCHS* new)
+int g_idechs2chs (tIDECHS g1, tCHS *g2)
 {
+    tLBA temp;
 
+    g_idechs2lba(g1,&temp);
+
+    if (!g_lba2chs(temp,g2)) return 0;
+
+    return 1;
 }
 
-int g_large2idechs (tLARGE old, tIDECHS* new)
+int g_idechs2lagre (tIDECHS g1, tLARGE *g2)
 {
+    tLBA temp;
 
+    g_idechs2lba(g1, &temp);
+
+    if (g_lba2large(temp,g2) == 0) return 0;
+
+    return 1;
 }
 
-int g_large2lba (tLARGE old, tLBA* new)
+int g_idechs2lba (tIDECHS g1, tLBA *g2)
 {
+    *g2 = g1.C * g1.H * g1.S;
 
+    return 1;
 }
 
-int g_idechs2chs (tIDECHS old, tCHS* new)
+int a_lba2chs (tCHS g1, tLBA a1, tCHS *a2)
 {
+    int a;
 
+    a2->C = a1 / (g1.H * g1.S);
+    a = a1 % (g1.H * g1.S);
+    a2->H = a / g1.S;
+    a2->S = a % g1.S;
+
+    return 1;
 }
 
-int g_idechs2lagre (tIDECHS old,  tLARGE* new)
+int a_lba2large (tLARGE g1, tLBA a1, tLARGE *a2)
 {
+    int a;
 
+    if ((g1.C*g1.H*g1.S)<a1) return 0;
+
+    a2->C = a1 / (g1.H * g1.S);
+
+    a = a1 % (g1.H * g1.S);
+
+    a2->H = a / g1.S;
+
+    a2->S = a % g1.S;
+
+    return 1;
 }
 
-int g_idechs2lba (tIDECHS old, tLBA* new)
+int a_lba2idechs (tIDECHS g1, tLBA a1, tIDECHS *a2)
 {
+    int a;
 
+    a2->C = a1 / (g1.H * g1.S);
+
+    a = a1 % (g1.H * g1.S);
+
+    a2->H = a / g1.S;
+
+    a2->S = a % g1.S;
+
+    return 1;
+}
+
+int a_chs2lba (tCHS g1, tCHS a1, tLBA *a2)
+{
+    if ((g1.C < a1.C) || (g1.H < a1.H) || (g1.S < a1.S)) return 0;
+
+    *a2 = a1.C * (g1.H * g1.S) + a1.H * g1.S + a1.S;
+
+    return 1;
+}
+
+int a_large2lba (tLARGE g1, tLARGE a1, tLBA *a2)
+{
+    if ((g1.C < a1.C) || (g1.H < a1.H) || (g1.S < a1.S)) return 0;
+
+    *a2 = a1.C * (g1.H * g1.S) + a1.H * g1.S + a1.S;
+
+    return 1;
+}
+
+int a_idechs2lba (tIDECHS g1, tIDECHS a1, tLBA *a2)
+{
+    if ((g1.C < a1.C) || (g1.H < a1.H) || (g1.S < a1.S)) return 0;
+
+    *a2 = a1.C * (g1.H * g1.S) + a1.H * g1.S + a1.S;
+
+    return 1;
+}
+
+int a_large2chs (tLARGE g1, tCHS g2, tLARGE a1, tCHS *a2)
+{
+    int c, h, k = 1;
+
+    if (g1.C >= g2.C || g1.H <= g2.H) return 0;
+
+    c = g1.C;
+    h = g1.H;
+
+    do {
+        c *= 2;
+        h /= 2;
+        k *= 2;
+    } while ((c < g2.C) && (h > g2.H));
+
+    a2->C = g1.C * (a1.H % k) + a1.C;
+    a2->H = a1.H % k;
+    a2->S = a1.S;
+
+    return 1;
+}
+
+int a_large2idechs (tLARGE g1, tIDECHS g2, tLARGE a1, tIDECHS *a2)
+{
+    int c, h, k = 1;
+
+    if (g1.C >= g2.C || g1.H <= g2.H) return 0;
+
+    c = g1.C;
+    h = g1.H;
+
+    do {
+        c *= 2;
+        h /= 2;
+        k *= 2;
+    } while ((c < g2.C) && (h > g2.H));
+
+    a2->C = g1.C * (a1.H % k) + a1.C;
+    a2->H = a1.H % k;
+    a2->S = a1.S;
+
+    return 1;
+}
+
+int a_chs2large (tCHS g1, tLARGE g2, tCHS a1, tLARGE *a2)
+{
+    int c, h, k=1;
+
+    if (g1.C <= g2.C || g1.H >= g2.H) return 0;
+
+    c = g1.C;
+    h = g1.H;
+
+    do {
+        c /= 2;
+        h *= 2;
+        k *= 2;
+    } while ((c > g2.C) && (h < g2.H));
+
+    a2->C = a1.C % g2.C;
+    a2->H = (g2.H / k) * (a1.C % k) + a1.H;
+    a2->S = a1.S;
+
+    return 1;
+}
+
+int a_idechs2large (tIDECHS g1, tLARGE g2, tIDECHS a1, tLARGE *a2)
+{
+    tLBA temp;
+
+    a_idechs2lba(g1, a1, &temp);
+
+    if (!a_lba2large(g2, temp, a2)) return 0;
+
+    return 1;
+}
+
+int a_chs2idechs (tCHS g1, tIDECHS g2, tCHS a1, tIDECHS *a2)
+{
+    int c,h,k=1;
+
+    if (g1.C >= g2.C || g1.H <= g2.H) return 0;
+
+    c = g1.C;
+    h = g1.H;
+
+    do {
+    c *= 2;
+    h /= 2;
+    k *= 2;
+    } while ((c < g2.C) && (h > g2.H));
+
+    a2->C = g1.C * (a1.H % k) + a1.C;
+    a2->H = a1.H % k;
+    a2->S = a1.S;
+
+    return 1;
+}
+
+int a_idechs2chs (tIDECHS g1, tCHS g2, tIDECHS a1, tCHS *a2)
+{
+    tLBA temp;
+
+    a_idechs2lba(g1, a1, &temp);
+
+    if (!a_lba2chs(g2, temp, a2)) return 0;
+
+    return 1;
 }
--- lab6/main.c
+++ lab6/main.c
@@ -1,20 +1,217 @@
 #include "lab7.h"
 
-
-int main()  
+struct partition_table 
 {
-    tIDECHS d;
+    tPartition table[4];
+    struct partition_table* next;
+} *first, *temp, *last; 
 
-    printf("\n Heads: ");
-    scanf("%hd", &d.H);
-    printf("\n Cylinders: ");
-    scanf("%hd", &d.C);
-    printf("\n Sectors: ");
-    scanf("%hd", &d.S);
+tPartition tm;
+tIDECHS geomet;
+tLARGE geom,adr;
+tLBA disk, adres = 1;
 
-    set_idechs_capacity(&d); 
+int main()
+{
 
-    printf("\n C: %hd H: %hd S: %hd\n Capacity: %.3lf\n\n", d.C, d.H, d.S, d.capacity );
+    int t;
+    int k = 0;
+    int b = 1;
+    int free_size; 
+    int type_oc; 
+    int bool_oc;
+    char c;
+
+    geom.C = 1023; 
+    geom.H = 255;
+    geom.S = 63;
+
+    first = malloc(sizeof(struct partition_table)); 
+
+    last = first;
+
+    printf("Input IDECHS geometry\n");
+
+    while(1) 
+    {
+        printf("C:");
+        scanf("%d", &t);
+
+        if(t > geom.C)  
+            printf("Error\n");
+        else break;    
+    }
+
+    geomet.C=t;
+
+    while(1) 
+    {
+        printf("H:");
+        scanf("%d", &t);
+
+        if(t > geom.C)  
+            printf("Error\n");
+        else break;  
+    }
+
+    geomet.H=t;
+
+    while(1) 
+    {
+        printf("S:");
+        scanf("%d",&t); 
+
+        if(t>geom.S)
+            printf("Error\n");
+        else break; 
+    }
+
+    geomet.S=t;
+
+    g_idechs2lba(geomet, &disk); 
+
+    printf("Disk sise: %.2f Gb\n", (float) disk / (1024 * 2048)); 
+
+    free_size = disk / 2;
+
+    do {
+
+        do {
+            printf("Free space: %d Кб\n",free_size); // выводим оставшееся место
+
+            printf("Section size (Кб): ");
+
+            scanf("%d", &t); 
+        } while (t > free_size); 
+
+        if(t == 0) break;
+
+        free_size -= t; 
+
+        tm.startLBA = adres; 
+        tm.size = t; 
+        adres += tm.size; 
+
+        if(a_lba2large(geom,tm.startLBA, &adr)) 
+            tm.startCHS = adr;
+        else
+            tm.startCHS=geom;
+
+        if (a_lba2large(geom,adres, &adr)) 
+            tm.endCHS=adr; 
+        else
+            bool_oc=1;
+
+        while (bool_oc)
+        {
+            bool_oc=0;
+
+            printf("Тип ОС:\n"); 
+            printf("1 - FAT16\n");
+            printf("2 - FAT32\n");
+            printf("3 - Linux swap\n");
+            printf("4 - Linux\n");
+            printf("5 - HPFS/NTFS\n");
+
+            scanf("%d",&type_oc);
+
+            switch (type_oc)
+            { 
+            case 1:
+                tm.type = 0x04;
+                break;
+
+            case 2:
+                tm.type = 0x0c;
+                break;
+
+            case 3:
+                tm.type = 0x82;
+                break;
+
+            case 4:
+                tm.type = 0x83;
+                break;
+
+            case 5:
+                tm.type = 0x07;
+                break;
+
+            default:
+                bool_oc = 1;
+            }
+        }
+
+        tm.active = 0;
+
+        if(b) {
+
+            printf("Set section as active (y/n): ");
+
+            do {
+                scanf("%c", &c);
+            } while((c != 'n') && (c != 'y'));
+
+            if ((c == 'y')) 
+            {
+                tm.active = 0x80;
+                b = 0;
+            }
+
+        }
+
+        if (((tm.startCHS.C==geom.C)&&(tm.startCHS.H==geom.H)&&(tm.startCHS.S==geom.S))||(k==3)) 
+        {
+            last->table[k]=tm;
+            last->table[k].type=0x05;
+
+            if (k!=3)
+                last->table[k+1].startLBA=0;
+            
+            temp = malloc(sizeof(struct partition_table));
+
+            last->next=temp;
+            last=temp;
+            last->table[0]=tm;
+            last->table[1].startLBA=0;
+
+            k=1;
+        } 
+        else
+        {
+            last->table[k]=tm;
+            last->table[++k].startLBA=0;
+        }
+
+    } while (free_size != 0);
+
+    printf("Active |   CHS start   |   ОС   |  CHS finish   |  LBA start  | Size\n");
+    
+    temp = first;
+
+    while (temp != NULL) 
+    { 
+        int i;
+
+        for (i=0;i<4;i++){
+
+        if (temp->table[i].startLBA==0) break;
+
+        printf("  %2x       %2d %2d %2d       %2x       %2d %2d %2d         %4ld         %d \n",   \
+                temp->table[i].active,                  \
+                temp->table[i].startCHS.C,              \
+                temp->table[i].startCHS.H,              \
+                temp->table[i].startCHS.S,              \
+                temp->table[i].type,                    \
+                temp->table[i].endCHS.C,                \
+                temp->table[i].endCHS.H,                \
+                temp->table[i].endCHS.S,                \
+                temp->table[i].startLBA,                \
+                temp->table[i].size); 
+        }
+
+    temp=temp->next; 
+    }
 
     return 0;
 }
